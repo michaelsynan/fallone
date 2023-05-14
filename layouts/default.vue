c57af9e5ff08764d27c46aa5cdf28ca95c3cb662
mikey <mforest@protonmail.com>
2023-05-13 22:10:32 -0400

    Initial commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,21 +5,11 @@ import '@/assets/css/main.css'
 
 <template>
   <div>
-    <navbar />
     <slot />
   </div>
 </template>
 
 
 <style scoped>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.15s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(.25rem);
-}
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
-<script lang="ts" setup></script>
-
 <template>
-  <welcome />
+  <div class="flex flex-col h-screen bg-stone-200">
+    <div class="flex flex-col p-4">
+      <h1 class="text-4xl font-bold">Greg Fallone</h1>
+      <h2 class="text-3xl font-bold">Handyman Extraordinaire</h2>
+    </div>
+    <div class="flex-grow"></div>
+    <div class="flex flex-row justify-center items-center pb-10 w-full">
+      <div class="mx-6">
+        <button class="border-2 border-black rounded-full p-4 cursor-pointer bg-white text-black hover:bg-black hover:text-white transition-colors duration-200 shadow-sm">Fix It</button>
+      </div>
+      <div class="mx-6">
+        <button class="border-2 border-black rounded-full p-4 cursor-pointer bg-white text-black hover:bg-black hover:text-white transition-colors duration-200 shadow-sm">Discuss It</button>
+      </div>
+    </div>
+  </div>
 </template>
-
-
-<style scoped></style>
